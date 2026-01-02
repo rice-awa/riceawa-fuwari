fabc0c85ca771b1b80bae24a70871d5ba9df611c
rice_awa <q77884590@163.com>
2026-01-02 19:50:40 +0800

    feat: 更新博客配置及支持文章作者自定义

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
+		enable: true, // Display a banner image at the top of each post	
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "rice-awa",
 	bio: "热爱技术,分享知识",
 	links: [
